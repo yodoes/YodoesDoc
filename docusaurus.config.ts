695163b7cb0c0276384ce37bd6f7ee45c7ed3f58
yodoes <yodoes.com@gmail.com>
2025-06-23 13:41:56 +0800

    自定义域名

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,17 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  // url: 'https://yodoes.github.io',
+  url: 'doc.yodoes.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
+  // baseUrl: '/YodoesDoc/',
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'yodoes', // Usually your GitHub org/user name.
+  projectName: 'YodoesDoc', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
